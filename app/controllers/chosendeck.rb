5ffdbaa21fe6473ea3de0b8e44a107c077235f35
Code Division <codedivision@11th.ventures>
2015-01-30 16:18:47 +0800

    update user validate

--- app/controllers/chosendeck.rb
+++ app/controllers/chosendeck.rb
@@ -2,3 +2,12 @@ get '/deck' do
 
   erb :deck
 end
+
+get '/deck/:deck' do
+  params[:deck]
+  erb :chosendeck
+end
+
+post '/deck/:deck/checkanswer' do
+
+end
--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,14 +2,14 @@ enable :sessions
 
 
 post '/login' do
-  user = User.where(username: params[:username], password: params[:password])
-      if user.nil? false
-        session[:user] = true
-        redirect to('/deck')
-      else
-        session[:user] = false
-        redirect to('/')
-      end
+  @user = User.where(username: params[:username]).first
+    if @user.password == params[:pwd]
+      session[:user] = true
+      redirect to('/deck')
+    else
+      session[:user] = false
+      redirect to('/')
+    end
 end
 
 get '/createaccount' do
